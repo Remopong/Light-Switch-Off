d3c27935fc19eaf42b83021fcc356ab615bfe599
remopong <46031860+remopong@users.noreply.github.com>
2019-03-17 22:39:13 +0100

    Delete PUSH GITHUB LIGHT.bat

--- PUSH GITHUB LIGHT.bat
+++
@@ -1,7 +0,0 @@
-@Echo OFF
-cd D:\Users\Remi\Desktop\Arduino_Light
-set /p commitName="Nom du commit (pas d'espaces): "
-@echo %commitName%
-Git add *
-Git commit -m %commitName%
-Git push origin master
