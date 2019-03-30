f70f8c82c6220df775ff63444d2c189aef944c3d
unknown <remopong@gmail.com>
2019-03-30 17:31:12 +0100

    Receiver_Updated

--- Arduino_Lum_Receiver/Arduino_Lum_Receiver.ino
+++ Arduino_Lum_Receiver/Arduino_Lum_Receiver.ino
@@ -1,22 +1,21 @@
 /*
- * 
-Radio    Arduino
-CE    -> 9
-CSN   -> 10 (Hardware SPI SS)
-MOSI  -> 11 (Hardware SPI MOSI)
-MISO  -> 12 (Hardware SPI MISO)
-SCK   -> 13 (Hardware SPI SCK)
-IRQ   -> No connection
-VCC   -> No more than 3.6 volts
-GND   -> GND
+
+  Radio    Arduino
+  CE    -> 9
+  CSN   -> 10 (Hardware SPI SS)
+  MOSI  -> 11 (Hardware SPI MOSI)
+  MISO  -> 12 (Hardware SPI MISO)
+  SCK   -> 13 (Hardware SPI SCK)
+  IRQ   -> No connection
+  VCC   -> No more than 3.6 volts
+  GND   -> GND
 
 */
 
 #include <SPI.h>
 #include <NRFLite.h>
 
-int out = 6;
-int in = 7;
+int in = 8;
 int buttonState = 0;
 
 const static uint8_t RADIO_ID = 2;       // Our radio's id.  The transmitter will send to this id.
@@ -26,12 +25,12 @@ const static uint8_t PIN_RADIO_CSN = 10;
 
 struct RadioPacket // Any packet up to 32 bytes can be sent.
 {
-    int parents;
+  int parents;
 };
 
 struct toSend
 {
-    int SwitchOff = 1;
+  int SwitchOff = 1;
 };
 
 NRFLite _radio;
@@ -41,42 +40,36 @@ toSend _sender;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(in, INPUT);
-  pinMode(out, OUTPUT);
+  pinMode(in, INPUT_PULLUP);
+
   buttonState = LOW;
-        if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
-    {
-        Serial.println("Cannot communicate with radio");
-    }
-    
+  if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+  {
+    Serial.println("Cannot communicate with radio");
+  }
+
 }
 
 void loop()
 {
 
-    buttonState = digitalRead(in);
+  buttonState = digitalRead(in);
 
-    if (buttonState == LOW) {
-      _sender.SwitchOff = 0;
-      while (_radio.hasData())
+  if (buttonState == LOW) {
+    _sender.SwitchOff = 0;
+    while (_radio.hasData())
     {
-      
-        _radio.readData(&_radioData);
-        Serial.print("Receiving = ");
-        String msg = "";
-        msg += _radioData.parents;
-        
-        Serial.println(msg);
-        
-        if(_radioData.parents == 1){
-        digitalWrite(out, HIGH);
-        }else{
-          digitalWrite(out, LOW);
-          }
-     }
-}else{
-  
-   _radio.send(DESTINATION_RADIO_ID, &_sender, sizeof(_sender));
-  Serial.println("Sending");
+
+      _radio.readData(&_radioData);
+      Serial.print("Receiving = ");
+      String msg = "";
+      msg += _radioData.parents;
+
+      Serial.println(msg);
+    }
+  } else {
+
+    _radio.send(DESTINATION_RADIO_ID, &_sender, sizeof(_sender));
+    Serial.println("Sending");
   }
 }
--- Arduino_Lum_Transmitter/Arduino_Lum_Transmitter.ino
+++ Arduino_Lum_Transmitter/Arduino_Lum_Transmitter.ino
@@ -1,14 +1,14 @@
 /*
 
-Radio    Arduino
-CE    -> 9
-CSN   -> 10 (Hardware SPI SS)
-MOSI  -> 11 (Hardware SPI MOSI)
-MISO  -> 12 (Hardware SPI MISO)
-SCK   -> 13 (Hardware SPI SCK)
-IRQ   -> No connection
-VCC   -> No more than 3.6 volts
-GND   -> GND
+  Radio    Arduino
+  CE    -> 9
+  CSN   -> 10 (Hardware SPI SS)
+  MOSI  -> 11 (Hardware SPI MOSI)
+  MISO  -> 12 (Hardware SPI MISO)
+  SCK   -> 13 (Hardware SPI SCK)
+  IRQ   -> No connection
+  VCC   -> No more than 3.6 volts
+  GND   -> GND
 
 */
 
@@ -18,6 +18,7 @@ GND   -> GND
 int pinRelay = 8;
 
 int lumiPin = A0;
+
 const static uint8_t RADIO_ID = 3;             // Our radio's id.
 const static uint8_t DESTINATION_RADIO_ID = 2; // Id of the radio we will transmit to.
 const static uint8_t PIN_RADIO_CE = 6;
@@ -27,12 +28,12 @@ int failedCount = 0;
 
 struct RadioPacket
 {
-    int parents = 0;
+  int parents = 0;
 };
 
 struct toSend
 {
-    int SwitchOff;
+  int SwitchOff;
 };
 
 NRFLite _radio;
@@ -44,75 +45,75 @@ void setup()
 
   pinMode(pinRelay, OUTPUT);
   digitalWrite(pinRelay, HIGH);
-  
-    Serial.begin(9600);
 
-          if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
-    {
-        Serial.println("Cannot communicate with radio");
-    }
-  
+  Serial.begin(9600);
+
+  if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+  {
+    Serial.println("Cannot communicate with radio");
+  }
+
 }
 
 void loop()
 {
   Serial.println(failedCount);
-   delay(1000);
-   
-    int lumiVal = analogRead(lumiPin);
-    lumiVal = 50;
-    
-        //Serial.println(lumiVal);
+  delay(1000);
 
-    
-    if(lumiVal >= 200){
-      
-       //Light on
-      _radioData.parents = 1;
+  int lumiVal = analogRead(lumiPin);
+  lumiVal = 50;
+
+  //Serial.println(lumiVal);
+
+
+  if (lumiVal >= 200) {
+
+    //Light on
+    _radioData.parents = 1;
     if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) // Note how '&' must be placed in front of the variable name.
     {
-        Serial.println(_radioData.parents);
-        failedCount = 0;
-    }else
+      Serial.println(_radioData.parents);
+      failedCount = 0;
+    } else
     {
-        Serial.println("Failed");
-        failedCount ++;
+      Serial.println("Failed");
+      failedCount ++;
     }
-       
-      
-}else{
-        _radioData.parents = 0;
-    
+
+
+  } else {
+    _radioData.parents = 0;
+
     if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) // Note how '&' must be placed in front of the variable name.
     {
-        Serial.println(_radioData.parents);
-        failedCount = 0;
-    }else
+      Serial.println(_radioData.parents);
+      failedCount = 0;
+    } else
     {
-        Serial.println("Failed");
-        failedCount ++;
+      Serial.println("Failed");
+      failedCount ++;
     }
   }
 
-    if(failedCount >= 10){
-      
-      Serial.println("Inside");
-      
-        if(_radio.hasData()){
-          Serial.println("Receive");
-        _radio.readData(&_sender);
-        String msg = "";
-        msg += _sender.SwitchOff;
-        Serial.println(msg);
-        
-        if(_sender.SwitchOff == 1){
+  if (failedCount >= 10) {
 
-          digitalWrite(pinRelay, LOW);
+    Serial.println("Inside");
+
+    if (_radio.hasData()) {
+      Serial.println("Receive");
+      _radio.readData(&_sender);
+      String msg = "";
+      msg += _sender.SwitchOff;
+      Serial.println(msg);
+
+      if (_sender.SwitchOff == 1) {
+
+        digitalWrite(pinRelay, LOW);
 
         Serial.println("Action");
 
-        
-         }     
+
       }
-   }
+    }
+  }
 }
